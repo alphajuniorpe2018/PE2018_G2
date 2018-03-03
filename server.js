3634de7aef64e2f11dfef02532caf57c6a2868b7
biacoronha <biacoronha@gmail.com>
2018-03-03 13:09:52 -0300

    Inicio pag ranking

--- server.js
+++ server.js
@@ -1,13 +1,7 @@
-console.log('server is starting');
-
 var express = require('express');
 
 var app = express();
 
-var server = app.listen(3000, listening);
-
-function listening(){
-    console.log("listening...");
-}
+var server = app.listen(3000);
 
 app.use(express.static('static'));
